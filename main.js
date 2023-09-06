2073cc3e7fbad583249d78080db3d337103a557f
Josell Vibar <vibarjosell@gmail.com>
2023-09-06 22:02:58 +0800

    create container and buttons and center all

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-let div = document.createElement('div');
-div.style.cssText = "width: 100px; height: 100px; background: red; color: white;";
-div.innerHTML = "sample";
+let container = document.querySelector('#section');
+let btnBlack = document.createElement('button');
+let btnGray = document.createElement('button');
+let btnRGB = document.createElement('button');
+let btnSize = document.createElement('button');
+let buttonsContainer = document.querySelector('buttons');
 
-document.querySelector(".container").appendChild(div);
+function createDivs(col, rows) {
+    for(let i = 0; i < (col * rows); i++){
+        let div = document.createElement('div');
+        div.style.border = "1px solid red";
+        container.style.gridTemplateColumns = `repeat(${col}, ifr)`;
+        container.style.gridTemplateRows = `repeat(${rows}, ifr)`;
+        container.appendChild(div).classList.add('box');
+    }
+}
+
+createDivs(16,16);
