167bf079a28a59433f6e220bd7fe99d9fb9b8094
Josell Vibar <vibarjosell@gmail.com>
2023-09-06 20:36:24 +0800

    create div for sample if its working hehe

---
+++ main.js
@@ -0,0 +1,5 @@
+let div = document.createElement('div');
+div.setAttribute = "width: 100px height: 100px background: red color: white";
+div.innerHTML = "sample";
+
+document.querySelector(".container").appendChild(div);
