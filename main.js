a3846b1eaaa811853c9c6a7296f702f70c1301e2
Josell Vibar <vibarjosell@gmail.com>
2023-09-07 21:16:47 +0800

    create 16x16 grid using js only

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-let container = document.querySelector('#section');
+let section = document.querySelector('#section');
+let container = document.querySelector('.container');
 let btnBlack = document.createElement('button');
 let btnGray = document.createElement('button');
 let btnRGB = document.createElement('button');
@@ -8,9 +9,9 @@ let buttonsContainer = document.querySelector('buttons');
 function createDivs(col, rows) {
     for(let i = 0; i < (col * rows); i++){
         let div = document.createElement('div');
-        div.style.border = "1px solid red";
-        container.style.gridTemplateColumns = `repeat(${col}, ifr)`;
-        container.style.gridTemplateRows = `repeat(${rows}, ifr)`;
+        div.style.border = "1px solid #000";
+        container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
         container.appendChild(div).classList.add('box');
     }
 }
